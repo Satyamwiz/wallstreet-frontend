d987e3a82cf7fee5936ef89d71156907c9055e76
Abhishek BHosale <abhisheksachinbhosale@gmail.com>
2024-04-01 20:30:06 +0530

    fix: fixed api service error throw bug

--- src/services/apis.js
+++ src/services/apis.js
@@ -4,17 +4,23 @@ const userService = {
     loginUser: (loginData) => {
         return axiosNoAuthInstance.post("/auth/token/login/", loginData)
         .then(res => res.data)
-        .catch(err => err.response)
+        .catch(err => {
+            throw err.response
+        })
     },
     logoutUser: () => {
         return axiosAuthInstance.post("/auth/token/logout/")
         .then(res => res.data)
-        .catch(err => err.response)
+        .catch(err => {
+            throw err.response
+        })
     },
     registerUser: (registerData) => {
         return axiosNoAuthInstance.post("/auth/users/", registerData)
         .then(res => res.data)
-        .catch(err => err.response)
+        .catch(err => {
+            throw err.response
+        })
     }
 }
 
@@ -22,12 +28,16 @@ const newsService = {
     getNews: () => {
         return axiosNoAuthInstance.get("/news/")
         .then(res => res.data)
-        .catch(err => err.response)
+        .catch(err => {
+            throw err.response
+        })
     },
     getNewsDetail: (id) => {
         return axiosNoAuthInstance.get(`/news/${id}`)
         .then(res => res.data)
-        .catch(err => err.response)
+        .catch(err => {
+            throw err.response
+        })
     }
 }
 
@@ -35,27 +45,37 @@ const stockService = {
     getStocks: () => {
         return axiosAuthInstance.get("/stocks/")
         .then(res => res.data)
-        .catch(err => err.response)
+        .catch(err => {
+            throw err.response
+        })
     },
     getStockDetail: (id) => {
         return axiosAuthInstance.get(`/stocks/${id}`)
         .then(res => res.data)
-        .catch(err => err.response)
+        .catch(err => {
+            throw err.response
+        })
     },
     buyStock: (id, buyOrderData) => {
         return axiosAuthInstance.post(`/stocks/buy/${id}`, buyOrderData)
         .then(res => res.data)
-        .catch(err => err.response)
+        .catch(err => {
+            throw err.response
+        })
     },
     sellStock: (id, sellOrderData) => {
         return axiosAuthInstance.post(`/stocks/sell/${id}`, sellOrderData)
         .then(res => res.data)
-        .catch(err => err.response)
+        .catch(err => {
+            throw err.response
+        })
     },
     getQuantity: (id) => {
         return axiosAuthInstance.get(`/availablequantity/${id}`)
         .then(res => res.data)
-        .catch(err => err.response)
+        .catch(err => {
+            throw err.response
+        })
     }
 }
 
@@ -63,17 +83,23 @@ const portfolioService = {
     getPortfolio: () => {
         return axiosAuthInstance.get("/portfolio/")
         .then(res => res.data)
-        .catch(err => err.response)
+        .catch(err => {
+            throw err.response
+        })
     },
     getCash: () => {
         return axiosAuthInstance.get("/cash/")
         .then(res => res.data)
-        .catch(err => err.response)
+        .catch(err => {
+            throw err.response
+        })
     },
     getTransactions: () => {
         return axiosAuthInstance.get("/transactions/")
         .then(res => res.data)
-        .catch(err => err.response)
+        .catch(err => {
+            throw err.response
+        })
     }
 }
 
@@ -81,7 +107,9 @@ const marketService = {
     checkMarketStatus: () => {
         return axiosNoAuthInstance.get("/market/")
         .then(res => res.data)
-        .catch(err => err.response)
+        .catch(err => {
+            throw err.response
+        })
     }
 }
 
