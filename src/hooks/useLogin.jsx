7c2264cec3094b5898dc1d14e1aa605447f7f901
Abhishek BHosale <abhisheksachinbhosale@gmail.com>
2024-04-18 00:31:12 +0530

    fix: added mainwebsite authentication

--- src/hooks/useLogin.jsx
+++ src/hooks/useLogin.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { useAuthContext } from "./useAuthContext";
 import { userService } from "../services/apis";
 import {toast} from 'react-toastify';
+import axios from "axios";
 
 export const useLogin = () => {
     const [error, setError] = useState(null);
@@ -12,6 +13,8 @@ export const useLogin = () => {
         const id = toast.loading("Please wait...");
         setLoading(true);
         setError(null);
+
+        /*
         userService
             .loginUser({
                 password: password,
@@ -28,6 +31,62 @@ export const useLogin = () => {
                 setError(err);
                 toast.update(id, { render: (err.data[Object.keys(err.data)[0]])[0], type: "error", isLoading: false, autoClose:4000 })
             });
+        */
+
+        userService
+        .loginUser({
+            password: password,
+            name: username,
+        })
+        .then((res) => {
+            localStorage.setItem("user", res.auth_token);
+            dispatch({ type: 'LOGIN', payload: res.auth_token });
+            setLoading(false);
+            toast.update(id, { render: "Logged in successfully !", type: "success", isLoading: false, autoClose:4000 })
+        })
+        .catch((err) => {
+            axios.post(`${import.meta.env.VITE_MAINWEB_BACKEND}verify/user/`, {
+                username: username,
+                password: password,
+                event: "Wallstreet",
+                is_team: false
+            })
+            .then((res)=>{
+                if(res.data.detail==="verified"){
+                    userService.registerUser({
+                        email: res.data.user.email,
+                        name: res.data.user.username,
+                        password: password,
+                        contact_no: res.data.user.phone,
+                        first_name: res.data.user.first_name,
+                        last_name: res.data.user.last_name,
+                    })
+                    .then((res) => {
+                        userService
+                        .loginUser({
+                            password: password,
+                            name: username,
+                        })
+                        .then((res) => {
+                            localStorage.setItem("user", res.auth_token);
+                            dispatch({ type: 'LOGIN', payload: res.auth_token });
+                            setLoading(false);
+                            toast.update(id, { render: "Logged in successfully !", type: "success", isLoading: false, autoClose:4000 })
+                        })
+                    })
+                    .catch((err) => {
+                        toast.update(id, { render: err.response.data.detail, type: "error", isLoading: false, autoClose:4000 })
+                    })
+                }
+            })
+            .catch((err)=>{
+                toast.update(id, { render: err.response.data.detail, type: "error", isLoading: false, autoClose:4000 })
+            })
+            setLoading(false);
+            setError(err);
+        });
+
+            
     };
 
     return { login, error, loading };
