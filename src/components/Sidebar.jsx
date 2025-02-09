fcf9f658374eba1b437d6521a7bc7665991f8f00
Shr9vaniii <shravanisawant1111@gmail.com>
2025-02-09 15:49:30 +0530

    Update Sidebar.jsx

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,7 +3,7 @@ import { NavLink } from "react-router-dom";
 import image from "../assets/stockwhite.svg";
 import { useAuthContext } from "../hooks/useAuthContext";
 import { useLogout } from "../hooks/useLogout";
-import { FaBook } from "react-icons/fa";
+import { FaBook, FaNewspaper } from "react-icons/fa";
 
 /**
  * This is the sidebar component which only rendered is user is logged in and on desktop devices
@@ -39,7 +39,7 @@ const Sidebar = () => {
             <li className="nav-item">
               <NavLink
                 to="/stocks"
-                className="nav-link align-middle px-0 py-4 bi bi-bar-chart navitems h3"
+                className="nav-link align-middle px-0 py-3 bi bi-bar-chart navitems h3 fs-4"
               >
                 {" "}
                 Stocks
@@ -62,7 +62,7 @@ const Sidebar = () => {
             <li className="nav-item">
               <NavLink
                 to="/portfolio"
-                className="nav-link align-middle px-0 py-4 navitems h3 bi bi-pie-chart"
+                className="nav-link align-middle px-0 py-3 navitems h3 bi bi-pie-chart fs-4"
               >
                 {" "}
                 Portfolio
@@ -73,7 +73,7 @@ const Sidebar = () => {
           <li className="nav-item">
             <NavLink
               to="/ranking"
-              className="nav-link align-middle px-0 py-4 navitems h3 bi bi-box-arrow-left bi bi-people"
+              className="nav-link align-middle px-0 py-3 navitems h3 bi bi-box-arrow-left bi bi-people fs-4"
             >
               {" "}
               Ranking
@@ -83,11 +83,22 @@ const Sidebar = () => {
           <li className="nav-item">
             <NavLink
               to="/rules"
-              className="nav-link align-middle px-0 py-4 navitems h3"
+              className="nav-link align-middle px-0 py-3 navitems h3 fs-4"
             >
               <FaBook className="me-2" /> {/* Add icon before text */}
               Rules
             </NavLink>
+          </li>
+
+          <li className="nav-item">
+            <a
+              href="https://www.example.com"
+              className="nav-link px-0 py-3 navitems fs-4"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaNewspaper className="me-2" /> News
+            </a>
           </li>
         </ul>
 
