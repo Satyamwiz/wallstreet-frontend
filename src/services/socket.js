0b8e83d67fd59ee871e5935f4dd9ca14fcea3476
satyamwiz78 <satyampravinmengshetti@gmail.com>
2025-02-16 00:30:06 +0530

    buy and sell modal

--- src/services/socket.js
+++ src/services/socket.js
@@ -1,7 +1,7 @@
 // src/services/socketService.js
 import { io } from "socket.io-client";
 
-const SOCKET_SERVER_URL = import.meta.env.SOCKET_SERVER_URL; // Replace with actual server URL
+const SOCKET_SERVER_URL = "http://3.111.114.201:3500" // Replace with actual server URL
 
 class SocketService {
   constructor() {
