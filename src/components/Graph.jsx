1a800a270e55799eea49de745d8d3939f22e1e68
satyamwiz78 <satyampravinmengshetti@gmail.com>
2025-02-17 19:05:42 +0530

    i cant see below commit

--- src/components/Graph.jsx
+++ src/components/Graph.jsx
@@ -115,6 +115,8 @@ const Graph = ({ companyName }) => { // Accept companyName as a prop
   };
 
   return (
+    //i should see here but i cant
+    
     <div style={{ width: "60vw", height: "60vh", padding: "20px", margin: "auto" }}>
       {/* <h2>{companyName} Market Data</h2> */}
       <div style={{ marginBottom: "10px" }}>
